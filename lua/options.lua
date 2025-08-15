a8b7d03fd9ecfc3f9b31bcf60540fdf23ebec3fe
Carlos Marques <chmarques93@gmail.com>
2025-08-14 22:19:50 -0300

    remove list

--- lua/options.lua
+++ lua/options.lua
@@ -61,8 +61,7 @@ vim.o.splitbelow = true
 --  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
 --   See `:help lua-options`
 --   and `:help lua-options-guide`
-vim.o.list = true
--- vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.o.list = false
 
 -- Preview substitutions live, as you type!
 vim.o.inccommand = "split"
